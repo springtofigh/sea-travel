5f757ce35852a1fd07a870ac3903c5362788ea23
BT <bahartofigh@gmail.com>
2022-10-15 23:03:04 +0330

    first commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Global variables
+const menuToggleIcon = document.getElementById('menu-toggle-icon');
+const navMobile = document.getElementById('nav');
+const headerElement = document.getElementById('header');
+const navLinks = document.querySelectorAll('.nav-mobile .list-link');
+console.log(navLinks);
+
+// Toggle navigation
+const toggleMenu = () => {
+    navMobile.classList.toggle('active');
+    headerElement.classList.toggle('active');
+}
+
+menuToggleIcon.addEventListener('click' , toggleMenu);
+
+// Add/remove header border bottom on scroll
+const headerScrollEvent = () => {
+    if (this.scrollY >= 30) {
+        headerElement.classList.add('active-scroll');
+    } else {
+        headerElement.classList.remove('active-scroll');
+    }
+}
+
+window.addEventListener('scroll' , headerScrollEvent);
+
+// Add selected link styles to the link clicked and close navigation when clicking the links
+navLinks.forEach(link => link.addEventListener('click' , () => {
+    navMobile.classList.remove('active');
+    headerElement.classList.remove('active');
+
+    let current = document.getElementsByClassName('current');
+    current[0].className = current[0].className.replace(' current' , "");
+    link.className += " current"
+}));
+
+// Scroll Reveal
+const scroll = ScrollReveal({
+        distance: '25px',
+        duration: 2500
+});
+
+scroll.reveal(`.image-left , .plan-trip` , {
+    origin:'left'
+});
+
+scroll.reveal(`.image-center` , {
+    origin:'bottom'
+});
+
+scroll.reveal(`.image-right, .popular-destinations-data ,.showcase-data` ,{
+    origin:'right'
+});
+
+scroll.reveal('.beach-data-wrapper', {
+    origin:'bottom',
+    interval: 250
+});
+
+scroll.reveal('.newsletter-container' , {
+    origin:'top'
+});
+
+scroll.reveal(`.footer-container-inner > div, .footer-sperator , .copyright`, {
+    origin:'bottom',
+    interval: 250
+});
